1131bd5848a9f72e49fe95bba6a34d91ac1d3f63
njokichristine <christinenjoki6900@gmail.com>
2023-02-10 17:47:46 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+  #setter
+  def name=(name)
+    @name=name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  #getter
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
 end
+
+# kelly = Dog.new
+# kelly.name="kelly"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
 
-end
+    #setter method here
+    def name=(name)
+      @name = name
+    end
+  
+    def job=(job)
+      @job = job
+    end
+    #getter method here
+    def name
+      @name
+    end
+  
+    def job
+      @job
+    end
+  end
+  
+  rose = Person.new
+  rose.name=("Rose")
